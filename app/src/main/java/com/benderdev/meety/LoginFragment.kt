c7295d5df16f44175716a7f4f33959b2b1b32f5a
Bartosz Jakubowski <bjakubowski.dev@gmail.com>
2019-09-16 22:51:40 +0200

    Code cleanup

--- app/src/main/java/com/benderdev/meety/LoginFragment.kt
+++ app/src/main/java/com/benderdev/meety/LoginFragment.kt
@@ -9,33 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_login.*
+//import com.google.firebase.auth.FirebaseAuth
+//import kotlinx.android.synthetic.main.activity_main.*
+//import kotlinx.android.synthetic.main.fragment_login.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,36 +26,18 @@ class LoginFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val buttonLogin = getView()?.findViewById<Button>(R.id.buttonLogin)
-        val login = getView()?.findViewById<EditText>(R.id.login)
-        val password = getView()?.findViewById<EditText>(R.id.password)
+        super.onViewCreated(view, savedInstanceState)
 
-        buttonLogin?.setOnClickListener{
+        val buttonLogin = view.findViewById<Button>(R.id.buttonLogin)
+        val login = view.findViewById<EditText>(R.id.login)
+        val password = view.findViewById<EditText>(R.id.password)
+
+        buttonLogin?.setOnClickListener {
             Log.d("Debug", "Login: ".plus(login?.text))
             Log.d("Debug", "Password: ".plus(password?.text))
+            //FirebaseAuth.getInstance().sign
         }
 
 
-
-        super.onViewCreated(view, savedInstanceState)
-    }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
