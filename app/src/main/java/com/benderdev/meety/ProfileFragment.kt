bc5f88168ecdf3ddf652ee3532e20b81dc23fafc
Bartosz Jakubowski <bjakubowski.dev@gmail.com>
2019-09-20 15:27:43 +0200

    Add data queries and data saving to profile
    Yeah, everything working, added also exception handling for every outsourced stuff and temporary debug logs for easier debugging.

--- app/src/main/java/com/benderdev/meety/ProfileFragment.kt
+++ app/src/main/java/com/benderdev/meety/ProfileFragment.kt
@@ -3,14 +3,18 @@ package com.benderdev.meety
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.*
 
 
@@ -31,11 +35,56 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val textEmail = view.findViewById<TextView>(R.id.textEmail)
+        val editName = view.findViewById<EditText>(R.id.editName)
+        val editDescription = view.findViewById<EditText>(R.id.editDescription)
+        val textPoints = view.findViewById<TextView>(R.id.textPoints)
+
         val buttonSignOut = view.findViewById<Button>(R.id.buttonSignOut)
+        val buttonSave = view.findViewById<Button>(R.id.buttonSave)
+
 
         auth = FirebaseAuth.getInstance()
-
         textEmail.text = auth.currentUser?.email
+
+        val database = FirebaseFirestore.getInstance()
+        val reference = database.collection("users").document(auth.uid!!)
+        reference.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("Debug", "DocumentSnapshot data: ${document.data}")
+
+                    editName.setText(document.data!!["name"].toString())
+                    editDescription.setText(document.data!!["description"].toString())
+                    textPoints.text = document.data!!["points"].toString()
+
+                } else {
+                    Log.d("Debug", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("Debug", "Getting document failed. Following exception occurred: ", exception)
+            }
+
+
+
+
+
+
+
+
+
+        buttonSave.setOnClickListener {
+            val data = hashMapOf("name" to editName.text.toString(), "description" to editDescription.text.toString())
+
+            reference.update(data as Map<String, Any>)
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Successfully updated data", Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(context, "Data update failed", Toast.LENGTH_LONG).show()
+                    Log.d("Debug", "Data update failed. Following exception occurred: ", exception)
+                }
+        }
 
         buttonSignOut.setOnClickListener {
             auth.signOut()
